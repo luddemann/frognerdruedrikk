6802b797c04513db76b1daddc5d08136f66f2b50
Ludvik <ludvik.blunck@aschehoug.no>
2024-09-24 09:55:18 +0200

    about page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <header class="max-w-screen-xl w-full mx-auto px-4 sm:px-8 border-b border-b-primary">
-    <nav class="py-8 max-w-screen-lg mx-auto">
+  <header class="max-w-screen-xl w-full mx-auto px-4 sm:px-8 pt-8">
+    <nav class="max-w-screen-lg mx-auto mb-8">
       <ul class="hidden md:grid grid-cols-3 w-full justify-between items-center">
         <li class="mr-auto">
           <a class="hover:underline" href="#">NETTBUTIKK</a>
@@ -19,15 +19,15 @@
         </li>
       </ul>
       <div class="flex md:hidden gap-6 w-full flex-col items-center">
-          <router-link class="flex" to="/">
-            <img src="../assets/fdflogo.svg" alt="">
-          </router-link>
+        <router-link class="flex" to="/">
+          <img src="../assets/fdflogo.svg" alt="">
+        </router-link>
         <div class="flex gap-12">
           <a class="hover:underline" href="#">NETTBUTIKK</a>
           <router-link class="hover:underline" to="/om-oss">OM OSS</router-link>
         </div>
       </div>
     </nav>
-    
+    <hr aria-hidden="true" class="border border-b-primary">
   </header>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,5 +3,35 @@
 </script>
 
 <template>
-<h1 class="text-8xl text-yellow-800">About</h1>
+  <div class="my-20 space-y-16">
+    <section class="flex items-center flex-col gap-6 text-center">
+      <h2 class="text-4xl md:text-7xl uppercase text-primary font-standard">Om oss</h2>
+      <p class="max-w-[40rem] text-xl md:text-2xl font-standard">Sommeren 2020, i et kollektiv på Frogner i Oslo samlet en vennegjeng seg for å drikke vin. Utallige flasker, knuste glass og rødvinsflekker senere, bestemte de seg for å starte Frogner Druedrikk Forening. </p>
+    </section>
+    <hr class="border-primary">
+    <section class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center justify-center">
+      <article class="space-y-6 mx-auto">
+        <h3 class="uppercase font-standard text-3xl">Vår filosofi</h3>
+        <p class="flex flex-col gap-6 max-w-md">
+          <span>Konseptet er enkelt. Vin er godt. Men, vin er så mye bedre når man nyter det med andre.</span>
+          <span>Vi har som formål å ufarliggjøre vinkulturen, og eliminere snobberiet rundt denne fantastiske druedrikken vi alle elsker. Drikk det du synes er godt, uavhengig av opphav, prisklasse, og jordsmonn. </span>
+          <span>Hvis noen spør deg om noe vinteknisk tull, svarer du bestemet at “vi skal sørover”. Mest sannsynlig stemmer nok det uansett. Vin er til for å nytes, ikke analyseres.</span>
+        </p>
+      </article>
+      <img src="../assets/stemningsbilde.png" alt="">
+    </section>
+    <section class="py-20 text-center"><h2 class="text-3xl sm:text-4xl uppercase md:text-7xl font-standard">“Vi skal sørover”</h2></section>
+    <section class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center justify-center">
+      <img src="../assets/stemningsbilde.png" alt="">
+      <article class="space-y-6 mx-auto">
+        <h3 class="uppercase font-standard text-3xl">Vår filosofi</h3>
+        <p class="flex flex-col gap-6 max-w-md">
+          <span>Konseptet er enkelt. Vin er godt. Men, vin er så mye bedre når man nyter det med andre.</span>
+          <span>Vi har som formål å ufarliggjøre vinkulturen, og eliminere snobberiet rundt denne fantastiske druedrikken vi alle elsker. Drikk det du synes er godt, uavhengig av opphav, prisklasse, og jordsmonn. </span>
+          <span>Hvis noen spør deg om noe vinteknisk tull, svarer du bestemet at “vi skal sørover”. Mest sannsynlig stemmer nok det uansett. Vin er til for å nytes, ikke analyseres.</span>
+          <a href="#" class="bg-primary self-baseline text-[#F9F6EA] py-2 px-9 rounded-full hover:opacity-90 transition">Bestill t-skjorte</a>
+        </p>
+      </article>
+    </section>
+  </div>
 </template>
